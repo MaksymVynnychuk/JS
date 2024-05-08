86ef5da480063da30e7ebef36ec228674911755d
MaksymVynnychuk <137117476+MaksymVynnychuk@users.noreply.github.com>
2024-05-08 03:00:01 +0300

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -1,7 +1,7 @@
 (function() {
   var names = ["John", "Jane", "Jack", "Jill", "James", "Alice", "Bob"];
 
-  console.log("Дефолтний варіант");
+  console.log("Дефолтний варіант:");
   for (var i = 0; i < names.length; i++) {
       var name = names[i];
       var firstLetter = name.charAt(0).toLowerCase();
@@ -13,15 +13,14 @@
       }
   }
 
-  console.log("мій варіант")
+  console.log("Мій варіант:")
   for(var j=0; j<names.length; j++){
+    var name = names[j];
     if(j % 2 === 1){
         speakHello.speak(name);
     }
     else{
         speakGoodBye.speak(name)
-    }
+    } 
   }
 })();
-
-
